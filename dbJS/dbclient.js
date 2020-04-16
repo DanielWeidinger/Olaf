4088f7ef6881b6903c446cf2cc88b0ae3b1052b4
Daniel Weidinger <Daniel.Weidinger@hp-weidinger.at>
2020-04-16 11:31:42 +0200

    dockerization

--- dbJS/dbclient.js
+++ dbJS/dbclient.js
@@ -1,3 +1,4 @@
+console.log("da tanza is schlecht weil er ka location für nusskonsum hod")
 var mqtt = require('mqtt')
 var mongo = require('mongodb')
 var client = mqtt.connect('mqtt://192.168.99.100:1883')
@@ -6,42 +7,51 @@ var url = 'mongodb://192.168.99.100:27017/'
 
 client.on('connect', () => {
     console.log('connected')
-    client.subscribe('#', ()=>{
+    client.subscribe('#', () => {
         mongoClient.connect(url, (err, db) => {
-            if(err) throw err
+            if (err) throw err
             console.log('dbcreated')
             var dbo = db.db('testJS')
-            dbo.createCollection("history", (err, res)=>{
-                if(err) throw err
-                console.log("Colelctin created")
+            dbo.createCollection("history", (err, res) => {
+                if (err) throw err
+                console.log("Collectin created")
                 db.close()
             })
         })
     })
-    
+
 })
 
 client.on('message', (topic, payload) => {
     var data = payload.toString()
     mongoClient.connect(url, (err, db) => {
-        if(err) throw err
+        if (err) throw err
         var dbo = db.db("testJS")
         dbo.collection("history")
-        .insertOne({ "topic" : topic, "title": data}, (err, res)=>{
-            if(err) throw err
-            console.log("inserted")
-            db.close()
-        })
+            .insertOne({
+                "topic": topic,
+                "body": data
+            }, (err, res) => {
+                if (err) throw err
+                console.log("inserted")
+                db.close()
+            })
     })
 })
 
-function getInserts(topic){
+function getInserts(topic) {
     var result;
-    mongoClient.connect(url, (err, db)=>{
-        if(err) throw err
+    mongoClient.connect(url, (err, db) => {
+        if (err) throw err
         var dbo = db.db('testJS');
         result = dbo.collection("history")
-            .find({}, { projection: { _id: 0, topic: 1, title: 1 }}).toArray((err, data) =>{
+            .find({}, {
+                projection: {
+                    _id: 0,
+                    topic: 1,
+                    title: 1
+                }
+            }).toArray((err, data) => {
                 result = data;
                 console.log(res)
                 db.close();
--- dbJS/httpClient/httpServer.js
+++ dbJS/httpClient/httpServer.js
@@ -4,30 +4,40 @@ var mongo = require('mongodb')
 var mongoClient = mongo.MongoClient;
 var url = 'mongodb://192.168.99.100:27017/'
 
+console.log("josoosodjdsf")
+
 var httpServer = http.createServer((req, res) => {
     var data;
-    if(req.url === '/'){
+    if (req.url === '/') {
         res.end("Nothing much!")
-    }
-    else{
-        mongoClient.connect(url, (err , db) => {
+    } else {
+        mongoClient.connect(url, (err, db) => {
             var query;
-            if(req.url === "/all"){
-                query = { topic: /.*/}
+            if (req.url === "/all") {
+                query = {
+                    topic: /.*/
+                }
                 console.log("all_________________")
-            }
-            else{
+            } else {
                 var re = new RegExp(".*" + req.url + ".*/", 'i')
-                
-                query = {topic: re}
+
+                query = {
+                    topic: re
+                }
             }
             var dbo = db.db('testJS')
-            data = dbo.collection('history').find(query, { projection: { _id: 0, topic: 1, title: 1 }})
+            data = dbo.collection('history').find(query, {
+                    projection: {
+                        _id: 0,
+                        topic: 1,
+                        title: 1
+                    }
+                })
                 .toArray()
             data.then(v => {
-                 res.end(JSON.stringify(v))
+                res.end(JSON.stringify(v))
             })
-            
+
         })
     }
 });
@@ -42,9 +52,15 @@ console.log("listen http")
 
 var getEntries = async (topic) => {
     var data;
-    mongoClient.connect(url, (err , db) => {
+    mongoClient.connect(url, (err, db) => {
         var dbo = db.db('testJS')
-        data = dbo.collection('history').find({}, { projection: { _id: 0, topic: 1, title: 1 }})
+        data = dbo.collection('history').find({}, {
+                projection: {
+                    _id: 0,
+                    topic: 1,
+                    title: 1
+                }
+            })
             .toArray()
         console.log(data)
         return data;
